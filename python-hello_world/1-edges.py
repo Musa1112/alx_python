a7914e490e0ee9faf947060a080d874cb0e7c687
Musa1112 <iliyamusa700@gmail.com>
2023-07-16 10:57:49 +0100

    py

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -7,3 +7,14 @@ middle_word = word[1:-1]
 print("First 3 letters: {}".format(first_three_letters))
 print("Last 2 letters: {}".format(last_two_letters))
 print("Middle word: {}".format(middle_word))
+
+word = "Holberton"
+
+first_three_letters = word[:3]
+last_two_letters = word[-2:]
+middle_word = word[1:-1]
+# YOUR CODE GOES HERE
+
+print("First 3 letters: {}".format(word[:3]))
+print("Last 2 letters: {}".format(word[-2:]))
+print("Middle word: {}".format(word[1:-1]))
