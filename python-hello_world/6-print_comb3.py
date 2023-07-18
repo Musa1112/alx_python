81e96f05f04927c894eb92884f0a003d875423d9
Musa1112 <iliyamusa700@gmail.com>
2023-07-18 16:16:16 +0100

    py

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,5 +1,8 @@
 for i in range(10):
     for j in range(i + 1, 10):
-        print("{:d}{:d}, ".format(i, j), end='')
+        if i < 9:
+            print("{:d}{:d}, ".format(i, j), end='')
+        else:
+            print("{:d}{:d}".format(i, j))
 
-print("89")
+print("")
