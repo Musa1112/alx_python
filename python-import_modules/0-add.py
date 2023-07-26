5621f8a8603713414b656bef976fe66fe576d031
Musa1112 <iliyamusa700@gmail.com>
2023-07-26 20:49:52 +0100

    py

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,12 +1,6 @@
-# main.py
-a = 1
-b = 2
-
-# Import the add function from add_0.py
-from add_0 import add
-
-# Calculate the result
-result = add(a, b)
-
-# Print the formatted output
-print(str(a) + " + " + str(b) + " = " + str(result))
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
