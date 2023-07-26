1edb239935dccc83bd9b510c5c0ebc61899c1c1c
Musa1112 <iliyamusa700@gmail.com>
2023-07-26 21:07:25 +0100

    py

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,6 +1,19 @@
-import sys
+#!/usr/bin/python3
+def print_arg(argv):
+    n = len(argv) - 1
+    if n == 0:
+        print("{:d} argument.".format(n))
+        return
+    else:
+        if n == 1:
+            print("{:d} argument:".format(n))
+        else:
+            print("{:d} arguments:".format(n))
+        i = 1
+        while i <= n:
+            print("{:d}: {:s}".format(i, argv[i]))
+            i += 1
 
 if __name__ == "__main__":
-    n = len(sys.argv) - 1
-    s = "s" if n != 1 else ""
-    print(f"{n} argument{s}: {', '.join(f'{i}: {arg}' for i, arg in enumerate(sys.argv[1:], 1))}." if n else "No arguments.")
+    import sys
+    print_arg(sys.argv)
