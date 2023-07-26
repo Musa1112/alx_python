28136fa4a11f3401c3ad21dba2ea0fdf4e7fe31d
Musa1112 <iliyamusa700@gmail.com>
2023-07-26 22:00:16 +0100

    py

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -1,13 +1,12 @@
-def safe_print_division(x, y):
-  """Safely prints the division of x and y."""
+def safe_print_division(a, b):
+  """Safely prints the division of a and b."""
   try:
-    result = x / y
-    print("{} / {} = {}".format(x, y, result))
+    result = a / b
+    print("Inside result: {}".format(result))
+    return result
   except ZeroDivisionError:
-    print("Cannot divide {} by 0".format(x))
-  finally:
-    print("This code is always executed.")
+    return None
 
 if __name__ == "__main__":
-  safe_print_division(10, 2)
-  safe_print_division(10, 0)
+  result = safe_print_division(10, 2)
+  print(result)
