22da6dce38bf64296c21995f96550eca98430a7b
Musa1112 <iliyamusa700@gmail.com>
2023-07-26 20:34:48 +0100

    py

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -2,8 +2,10 @@
 def add(a, b):
     return a + b
 
+    
+#!/usr/bin/python3
 if __name__ == "__main__":
+    from add_0 import add
     a = 1
     b = 2
-    result = add(a, b)
-    
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
