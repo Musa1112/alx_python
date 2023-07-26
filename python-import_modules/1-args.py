c45e3aab4f1cfc9e62e98b89aac6fbd83f13fa53
Musa1112 <iliyamusa700@gmail.com>
2023-07-26 21:05:21 +0100

    py

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,16 +1,6 @@
 import sys
 
 if __name__ == "__main__":
-    num_args = len(sys.argv) - 1
-
-    if num_args == 0:
-        print("0 argument.")
-    else:
-        if num_args == 1:
-            print(f"1 argument:")
-        else:
-            print(f"{num_args} arguments:")
-
-        for i, arg in enumerate(sys.argv[1:], start=1):
-            print(f"{i}: {arg}")
-
+    n = len(sys.argv) - 1
+    s = "s" if n != 1 else ""
+    print(f"{n} argument{s}: {', '.join(f'{i}: {arg}' for i, arg in enumerate(sys.argv[1:], 1))}." if n else "No arguments.")
