f55bb5f0d7657ffabdcc270f0733b2680adc105e
Musa1112 <iliyamusa700@gmail.com>
2023-08-12 12:10:11 +0100

    py

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -1,7 +1,5 @@
 def first_last_digit(number):
-  first_digit = number // 10 ** len(number)
-  last_digit = number % 10
-  return first_digit, last_digit
+  return (number // 10**len(str(number)) % 10, number % 10)
 
 if __name__ == "__main__":
   print(first_last_digit(98742))
