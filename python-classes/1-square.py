c1aa58afaae73aa1634a4131b73fea3c09bf20a9
Musa1112 <iliyamusa700@gmail.com>
2023-09-22 14:37:30 +0100

    py

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,33 +1,120 @@
 class Square:
-    """A class that defines a square."""
+    """Represents a square."""
 
     def __init__(self, size=0):
-        """Initialize the square with the given size."""
+        """Initialize a square.
+
+        Args:
+            size (int, optional): The size of the square. Defaults to 0.
+
+        Raises:
+            TypeError: If `size` is not an integer.
+            ValueError: If `size` is less than 0.
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        if size < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
 
+    def area(self):
+        """Calculate the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
+        return self.__size ** 2
+
     @property
     def size(self):
-        """Get the size of the square."""
+        """Get the size of the square.
+
+        Returns:
+            int: The size of the square.
+        """
         return self.__size
 
     @size.setter
-    def size(self, new_size):
-        """Set the size of the square."""
-        if not isinstance(new_size, int):
+    def size(self, value):
+        """Set the size of the square.
+
+        Args:
+            value (int): The new size of the square.
+
+        Raises:
+            TypeError: If `value` is not an integer.
+            ValueError: If `value` is less than 0.
+        """
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        if new_size < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = new_size
+        self.__size = value
+
+    def __str__(self):
+        """Return a string representation of the square.
+
+        Returns:
+            str: A string representation of the square.
+        """
+        return f"Square({self.__size})"
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size=0):
+        """Initialize a square.
+
+        Args:
+            size (int, optional): The size of the square. Defaults to 0.
+
+        Raises:
+            TypeError: If `size` is not an integer.
+            ValueError: If `size` is less than 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
 
     def area(self):
-        """Calculate the area of the square."""
-        return self.__size * self.__size
-# Documentation for the Square class
+        """Calculate the area of the square.
 
-    """This class defines a square with a private size attribute. The size attribute cannot be changed directly, but it can be accessed through the size property. The area method calculates and returns the area of the square."""
+        Returns:
+            int: The area of the square.
+        """
+        return self.__size ** 2
 
+    @property
+    def size(self):
+        """Get the size of the square.
 
+        Returns:
+            int: The size of the square.
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the square.
+
+        Args:
+            value (int): The new size of the square.
+
+        Raises:
+            TypeError: If `value` is not an integer.
+            ValueError: If `value` is less than 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def __str__(self):
+        """Return a string representation of the square.
+
+        Returns:
+            str: A string representation of the square.
+        """
+        return f"Square({self.__size})"
