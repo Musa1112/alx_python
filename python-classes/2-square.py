2a0fab1fb631e7456945c47c0880a9e05eba087c
Musa1112 <iliyamusa700@gmail.com>
2023-09-22 16:16:33 +0100

    py

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,19 +1,21 @@
-class Square:
-    """A class that defines a square."""
+#!/usr/bin/python3
+"""Defines a 'Square' class with a private instance attribute"""
 
+
+class Square:
+    """Definition of 'Square'"""
     def __init__(self, size=0):
-        """Initialize the square with the given size."""
+        """Inits Square with a size integer
+
+            Args:
+                size: An integer size of the square.
+
+            Raises:
+                TypeError: The size argument is not an integer.
+                ValueError: The size argument is negative.
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-
-    def area(self):
-        """Calculate the area of the square."""
-        return self.__size * self.__size
-
-# Documentation for the Square class
-
-    """This class defines a square with a private size attribute. The size attribute cannot be changed directly, but it can be accessed through the size property. The area method calculates and returns the area of the square."""
-
