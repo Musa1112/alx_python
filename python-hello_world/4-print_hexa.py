62d58ea20445d5964aba2c212cacecb90d56de6b
Musa1112 <iliyamusa700@gmail.com>
2023-10-15 16:36:34 +0100

    python_hello-world

--- python-hello_world/4-print_hexa.py
+++ python-hello_world/4-print_hexa.py
@@ -1,2 +1,6 @@
-for i in range(99):
-    print( "{} = {}".format( i , hex(i) ) )
+def main():
+  for i in range(100):
+    print("{:02d} {:02x}".format(i, i))
+
+if __name__ == "__main__":
+  main()
